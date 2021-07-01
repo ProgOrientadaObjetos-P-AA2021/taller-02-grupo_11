7518e19466eaf79e078a3ae775e67f72c5814e2d
MICHAEL EDUARDO MALDONADO PEREIRA <memaldonado28@utpl.edu.ec>
2021-07-01 15:53:59 -0500

    Trabajo Grupal

--- ProyectoTaller/src/paquete2/PasajeUniversitario.java
+++ ProyectoTaller/src/paquete2/PasajeUniversitario.java
@@ -12,8 +12,23 @@ import java.io.Serializable;
  * @author reroes
  */
 public class PasajeUniversitario extends PasajeInterCantonal {
-    
-    
-    
-    
+
+    String nomUni;
+///****************************
+    public String getNombreunivercidad() {
+        return nomUni;
+    }
+
+    public void setNombreUnivercidad(String nomUni) {
+        this.nomUni = nomUni;
+    }
+
+    @Override
+    public void setValorP() {
+        valorP = (tarifaB / 100);
+    }
+
+    public double getValorP() {
+        return valorP;
+    }
 }
